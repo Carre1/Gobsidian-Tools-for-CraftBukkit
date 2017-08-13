7a55f3a10ad4224486fc063fce32a752f7e7f82d
Hateful Carre1 <carre1@yeah.net>
2017-08-13 11:31:54 +0800

    Blank plugin

---
+++ src/com/gobsidian/toolsForBukkit/Main.java
@@ -0,0 +1,19 @@
+package com.carre1.firstbkplugin;
+
+/**
+ * Created by carre on 2017/8/13.
+ * FirstBukkitPlugins
+ */
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        System.out.println("Hello,world!");
+    }
+    @Override
+    public void onDisable(){
+        System.out.println("");
+    }
+}
